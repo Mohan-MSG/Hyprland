6a821fb381220401bf5183be278588225a11ea65
Mohan-MSG <mohansrinivas464@gmail.com>
2025-07-27 17:56:22 +0530

    Update Programs.sh

--- Programs.sh
+++ Programs.sh
@@ -2,23 +2,17 @@
 
 # Define the list of packages to install
 packages=(
-  firefox
-  kdeconnect
   torbrowser-launcher
-  vesktop
-  obsidian
   uv
-  spotify
   vlc
   libreoffice
-  thunar
-  whatsdesk
-  telegram-desktop-bin
+  brave-bin
   vlc
+  windsurf
+  duf
 )
 
 # Loop through each package and install using yay
 for package in "${packages[@]}"; do
   yay -S --noconfirm "$package"
 done
-
